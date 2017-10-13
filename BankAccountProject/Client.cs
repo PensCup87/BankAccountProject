69808c3084b1960b1f9c1a9a86078b22dd481564
PensCup87 <bdt9123@gmail.com>
2017-10-13 13:30:38 -0400

    Main menu, Client Class & Method

---
+++ BankAccountProject/Client.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountProject
+{
+    class Client
+    {
+        //fields
+        protected string firstName;
+        protected string lastName;
+        protected int bankAccountNumber;
+        
+
+
+        //Properties
+
+        private string FirstName
+        {
+            get { return this.firstName; }
+        }
+
+        private string LastName
+        {
+            get { return this.lastName; }
+        }
+
+        private int BankAccountNumber
+        {
+            get { return this.bankAccountNumber; }
+            
+        }
+
+        
+        //Constructor
+        public Client()
+        {
+            //default Const.
+            this.firstName = "Harley";
+            this.lastName = "Davidson";
+            this.bankAccountNumber = 1903;
+        }
+
+        public Client(string firstName, string lastName, int bankAccountNumber)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.bankAccountNumber = bankAccountNumber;
+            
+        }
+
+        //Method Time
+
+        public virtual void GetCustomerInfo()
+        {
+
+            Console.WriteLine("Hello " + firstName + " " + lastName);
+            Console.WriteLine("Your bank account number is: " + bankAccountNumber);
+        }
+
+
+    }
+}
--- BankAccountProject/Program.cs
+++ BankAccountProject/Program.cs
@@ -10,6 +10,40 @@ namespace BankAccountProject
     {
         static void Main(string[] args)
         {
+
+
+            Client newClient = new Client();
+
+
+
+
+                //Cold open
+                Console.WriteLine("Please select the number from the following options:");
+                Console.WriteLine("1 View Client Information");
+                Console.WriteLine("2 View Account Balabce");
+                Console.WriteLine("3 Deposit Funds");
+                Console.WriteLine("4 Withdraw Funds");
+                Console.WriteLine("5 Exit");
+
+            string userChoice = Console.ReadLine().Trim();
+            if(userChoice == "1")
+            {
+                newClient.GetCustomerInfo();
+            }
+            if(userChoice == "2")
+            {
+
+            }
+            if(userChoice == "3")
+            {
+
+            }
+            if(userChoice == "4")
+            {
+
+            }
+
+
         }
     }
 }
